813f2371dc7461cae327ca3a749eb007588ac284
Daniel Rocha <daniel1990rocha@gmail.com>
2021-08-30 17:56:38 -0300

    Handling Pagy::OverflowError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger, :warning, :info
+
+  rescue_from Pagy::OverflowError, with: :redirect_to_404
+
+  def redirect_to_404
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
