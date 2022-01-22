9263c4bcad6537a20b5a7f4f88e31ce35612c68a
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2022-01-22 09:40:18 -0300

    💎 Upgrade dependencies (#109)

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,5 +4,5 @@ require 'pagy'
 require 'pagy/extras/bootstrap'
 require 'pagy/extras/i18n'
 
-Pagy::VARS[:link_extra] = 'class="btn font-weight-bold"'
-Pagy::VARS[:size] = [0, 0, 0, 0]
+Pagy::DEFAULT[:link_extra] = 'class="btn font-weight-bold"'
+Pagy::DEFAULT[:size] = [0, 0, 0, 0]
