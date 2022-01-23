65b20199c02956e4bb6b161e9cd86f78f59d4366
Stephann <3025661+stephannv@users.noreply.github.com>
2022-01-23 13:19:28 -0300

    ✨ Add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_190230) do
+ActiveRecord::Schema.define(version: 2022_01_23_161406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_190230) do
     t.string "profile_image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
