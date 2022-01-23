96b31ce18215286e30c9ddd42eadd501e75702c5
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2022-01-23 10:03:59 -0300

    ✨ Improve game page (#113)

---
+++ spec/components/games/price_with_cta_button_component_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Games::PriceWithCTAButtonComponent, type: :component do
+  subject(:rendered) { render_inline(described_class.new(price: price)) }
+
+  context 'when price is nil' do
+    let(:price) { nil }
+
+    it 'doesn`t render' do
+      content = rendered.to_html
+      expect(content).to be_blank
+    end
+  end
+
+  context 'when price has discount' do
+    let(:price) { build(:price, :with_discount) }
+
+    it 'renders without problem' do
+      content = rendered.to_html
+      expect(content).to be_present
+    end
+  end
+
+  context 'when price doesn`t have discount' do
+    let(:price) { build(:price) }
+
+    it 'renders without problem' do
+      content = rendered.to_html
+      expect(content).to be_present
+    end
+  end
+end
