470c532d6c467064a1311bcad1d69e679bf013aa
Stephann <3025661+stephannv@users.noreply.github.com>
2022-03-01 14:58:08 -0300

    🐞 Parse HTML entities when building text message for Telegram

--- spec/lib/telegram_event_text_builder_spec.rb
+++ spec/lib/telegram_event_text_builder_spec.rb
@@ -14,15 +14,16 @@ RSpec.describe TelegramEventTextBuilder, type: :lib do
         old_price: Faker::Lorem.word,
         state: Faker::Lorem.word
       }
-      create(:item_event, data: event_data)
+      create(:item_event, title: item_event_title, data: event_data)
     end
+    let(:item_event_title) { "Game's Name & #123 <SERVER ERROR>" }
     let(:data) { item_event.data }
 
     it 'builds telegram text using item event data' do
       emoji = item_event.event_type_object.emoji
       expected_response = <<~HEREDOC
         #{emoji} <b>#{item_event.event_type_humanize}</b> #{emoji}
-        🕹 <b>#{item_event.title}</b>
+        🕹 <b>Game's Name &amp; #123 &lt;SERVER ERROR&gt;</b>
         📆 #{data['release_date']}
         💵 #{data['current_price']} <s>#{data['base_price']}</s> (#{data['discount_percentage']})
         📢 #{data['state']}
@@ -35,13 +36,13 @@ RSpec.describe TelegramEventTextBuilder, type: :lib do
     end
 
     context 'when non-required data is blank' do
-      let(:item_event) { create(:item_event) }
+      let(:item_event) { create(:item_event, title: item_event_title) }
 
       it 'builds telegram text with required data only' do
         emoji = item_event.event_type_object.emoji
         expected_response = <<~HEREDOC
           #{emoji} <b>#{item_event.event_type_humanize}</b> #{emoji}
-          🕹 <b>#{item_event.title}</b>
+          🕹 <b>Game's Name &amp; #123 &lt;SERVER ERROR&gt;</b>
 
           🔗 #{item_event.url}
         HEREDOC
