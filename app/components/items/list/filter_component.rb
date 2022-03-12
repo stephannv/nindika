6c36cc6d0da8c5f2f3f8327263ea79c486d30e0c
Stephann Vasconcelos <3025661+stephannv@users.noreply.github.com>
2022-03-12 18:55:20 -0300

    ✨ Bring back disabled filter checkbox for some pages (#133)

--- app/components/items/list/filter_component.rb
+++ app/components/items/list/filter_component.rb
@@ -16,4 +16,21 @@ class Items::List::FilterComponent < ViewComponent::Base
   def languages_options
     languages.map { |l| [I18nData.languages('PT-BR')[l], l] }
   end
+
+  def readonly_checkbox?(checkbox_id)
+    readonly_checkbox == checkbox_id
+  end
+
+  def readonly_checkbox
+    @readonly_checkbox ||= mapped_paths_for_readonly_checkbox[request.path]
+  end
+
+  def mapped_paths_for_readonly_checkbox
+    {
+      on_sale_games_path => :on_sale,
+      pre_order_games_path => :pre_order,
+      coming_soon_games_path => :coming_soon,
+      new_releases_games_path => :new_release
+    }
+  end
 end
