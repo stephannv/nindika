c84d26b4d6547af042ebb68d2bc858b1a1c19e51
stephann <3025661+stephannv@users.noreply.github.com>
2022-06-12 14:46:53 -0300

    🔧 Setup tailwind & daisyUI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs"
 import { Turbo } from "@hotwired/turbo-rails"
-import UIkit from "uikit"
 import "controllers"
 
 Rails.start()
-window.UIkit = UIkit
