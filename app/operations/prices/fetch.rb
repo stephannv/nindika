986cb8a84eec6dd9c8f5f4d7e77bdaf625ef7bff
stephann <3025661+stephannv@users.noreply.github.com>
2022-06-26 16:55:51 -0300

    🐞 Fix price import when nsuid isn't valid

--- app/operations/prices/fetch.rb
+++ app/operations/prices/fetch.rb
@@ -7,14 +7,41 @@ module Prices
     output :prices_data, type: Array
 
     def call
-      prices_data = []
+      self.prices_data = []
 
       Item.with_nsuid.find_in_batches(batch_size: 99) do |batch|
-        prices_data += client.fetch(country: "BR", lang: "pt", nsuids: batch.pluck(:nsuid))
-        sleep 1
+        nsuids = batch.pluck(:nsuid)
+        response = fetch_data(nsuids)
+
+        if response.is_a?(Array)
+          prices_data.concat response
+        else
+          prices_data.concat fetch_data_individually(nsuids)
+        end
+      end
+    end
+
+    private
+
+    def fetch_data(nsuids)
+      client.fetch(country: "BR", lang: "pt", nsuids: nsuids)
+    end
+
+    # Some NSUID returns a html page titled "The page you requested is not available. - Nintendo"
+    # so we need to iterated each nsuid from batch to get price skipping nsuid with error
+    def fetch_data_individually(nsuids)
+      data = nsuids.map do |nsuid|
+        response = fetch_data([nsuid])
+
+        if response.is_a?(Array)
+          response
+        else
+          Sentry.capture_message("NSUID WITH PRICE ERROR", level: :fatal, extra: { nsuid: nsuid })
+          nil
+        end
       end
 
-      self.prices_data = prices_data
+      data.compact
     end
   end
 end
